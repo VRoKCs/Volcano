f70d81d4c7edf7d6ea2505548fafa98908304774
plyo2273 <plyo2273@lps53.org>
2017-12-17 08:22:21 -0600

    initial commit
    volcano project

---
+++ Volcano/Assets/ToggleDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleDisplay : MonoBehaviour {
+
+	public GameObject info;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			info.SetActive (true);
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			info.SetActive (false);
+
+		}
+			
+	}
+}
---
+++ Volcano/Assets/adjustZ.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class adjustZ : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	/*
+	public void setY(string input) {
+		gameObject.transform.position.y = (float)input;
+}
+*/
+
+}
